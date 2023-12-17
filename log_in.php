2ea8141a1a41402bcd6c1e3a78c412d219067a63
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-12-17 01:04:02 -0500

    Changes to style

--- log_in.php
+++ log_in.php
@@ -1,22 +1,14 @@
 <!DOCTYPE html>
-<?php
-// session_start();
-//     include "connection.php";
-//     include "functions.php";
-//     // $user_data = checkLoginStatus();
-//     $user_data = check_login($conn);
-?>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Home</title>
+        <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="">
     </head>
     <body>
-        <h1>Welcome <php  ?></h1>
         
         <script src="" async defer></script>
     </body>
