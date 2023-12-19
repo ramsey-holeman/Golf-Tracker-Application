d72dc0d4380b08e319ff7a626fd4d69159d612c5
Ramsey Holeman <ramsey.holeman.19@cnu.edu>
2023-12-18 19:33:00 -0500

    Log out page fixed

--- log_out.php
+++ log_out.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        
-        <script src="" async defer></script>
-    </body>
-</html>
+<?php
+session_start();
+
+// If session is active then unset the session variable
+if(isset($_SESSION['user_id'])){
+    unset($_SESSION['user_id']);
+}
+
+header("Location: login_page.php");
+die();
+
+?>
